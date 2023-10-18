bf1b8b5018a71ab7929c9abd6b3da84549129178
Ryan Denlinger <denlinger.ryan@gmail.com>
2023-10-17 20:15:28 -0500

    LLP initializer

---
+++ src/LLPInitializer.java
@@ -0,0 +1,8 @@
+public interface LLPInitializer {
+    /**
+     * Create the initial lattice state for LLP.
+     * 
+     * @return The initial lattice state.
+     */
+    public int[] createInitialLatticeState();
+}
