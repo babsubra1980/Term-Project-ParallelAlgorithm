36800debeebe1a2668b78165775cabdc4785c906
Ryan Denlinger <denlinger.ryan@gmail.com>
2023-10-21 18:40:38 -0500

    wip: llp runner implementation

--- src/LLPRunner.java
+++ src/LLPRunner.java
@@ -6,22 +6,51 @@ public class LLPRunner<TInput, TOutput> {
     private final TInput input;
     private final LLPOutputBuilder<TInput, TOutput> llpOutputBuilder;
     private final int[] latticeValues;
+    private List<Thread> threads;
+    private final int numThreads;
+    private final DispatchAlgorithm dispatchAlgorithm;
+
+    public static enum DispatchAlgorithm {
+        /**
+         * Cycle through the threads, adding a worker to each in succession (this is the
+         * default).
+         */
+        ROUND_ROBIN,
+
+        /**
+         * Allocate workers to threads randomly.
+         */
+        RANDOM,
+
+        /**
+         * Split the workers into numThreads consecutive blocks and allocate each block
+         * to the same thread.
+         */
+        NAIVE
+    }
 
     private LLPRunner(List<LLPWorker<TInput>> workers, int[] latticeValues, TInput input,
-            LLPOutputBuilder<TInput, TOutput> llpOutputBuilder) {
+            LLPOutputBuilder<TInput, TOutput> llpOutputBuilder, int numThreads, DispatchAlgorithm dispatchAlgorithm) {
         this.workers = workers;
         this.latticeValues = latticeValues;
         this.input = input;
         this.llpOutputBuilder = llpOutputBuilder;
+        this.numThreads = numThreads;
+        this.dispatchAlgorithm = dispatchAlgorithm;
     }
 
-    public class LLPRunnerBuilder {
-        private LLPInitializer<TInput> initializer;
-        private TInput input;
-        private LLPOutputBuilder<TInput, TOutput> llpOutputBuilder;
-        private List<LLPWorker<TInput>> workers;
-        private LLPWorker<TInput> worker;
+    public static class LLPRunnerBuilder<SInput, SOutput> {
+        private LLPInitializer<SInput> initializer;
+        private SInput input;
+        private LLPOutputBuilder<SInput, SOutput> llpOutputBuilder;
+        private List<LLPWorker<SInput>> workers;
+        private LLPWorker<SInput> worker;
+        private int numThreads = 8;
+        private DispatchAlgorithm dispatchAlgorithm = DispatchAlgorithm.ROUND_ROBIN;
 
+        /**
+         * Builder constructor
+         */
         public LLPRunnerBuilder() {
         }
 
@@ -31,7 +60,7 @@ public class LLPRunner<TInput, TOutput> {
          * @param initializer The initializer
          * @return This builder
          */
-        public LLPRunnerBuilder setInitializer(LLPInitializer<TInput> initializer) {
+        public LLPRunnerBuilder<SInput, SOutput> setInitializer(LLPInitializer<SInput> initializer) {
             this.initializer = initializer;
             return this;
         }
@@ -42,8 +71,24 @@ public class LLPRunner<TInput, TOutput> {
          * @param input The input
          * @return This builder
          */
-        public LLPRunnerBuilder setInput(TInput input) {
+        public LLPRunnerBuilder<SInput, SOutput> setInput(SInput input) {
             this.input = input;
+            return this;
+        }
+
+        public LLPRunnerBuilder<SInput, SOutput> setDispatchAlgorithm(DispatchAlgorithm dispatchAlgorithm) {
+            this.dispatchAlgorithm = dispatchAlgorithm;
+            return this;
+        }
+
+        /**
+         * Set the number of threads (defaults to 8 if never called)
+         * 
+         * @param numThreads The number of threads
+         * @return This builder
+         */
+        public LLPRunnerBuilder<SInput, SOutput> setNumThreads(int numThreads) {
+            this.numThreads = numThreads;
             return this;
         }
 
@@ -53,7 +98,7 @@ public class LLPRunner<TInput, TOutput> {
          * @param worker A prototype of a worker
          * @return This builder
          */
-        public LLPRunnerBuilder setWorkerPrototype(LLPWorker<TInput> worker) {
+        public LLPRunnerBuilder<SInput, SOutput> setWorkerPrototype(LLPWorker<SInput> worker) {
             this.worker = worker;
             return this;
         }
@@ -64,7 +109,7 @@ public class LLPRunner<TInput, TOutput> {
          * @param llpOutputBuilder The output builder
          * @return This builder
          */
-        public LLPRunnerBuilder setOutputBuilder(LLPOutputBuilder<TInput, TOutput> llpOutputBuilder) {
+        public LLPRunnerBuilder<SInput, SOutput> setOutputBuilder(LLPOutputBuilder<SInput, SOutput> llpOutputBuilder) {
             this.llpOutputBuilder = llpOutputBuilder;
             return this;
         }
@@ -74,27 +119,39 @@ public class LLPRunner<TInput, TOutput> {
          * 
          * @return The runner
          */
-        public LLPRunner<TInput, TOutput> build() {
+        public LLPRunner<SInput, SOutput> build() {
             int[] latticeValues = initializer.createInitialLatticeState(input);
-            workers = new ArrayList<LLPWorker<TInput>>(latticeValues.length);
+            workers = new ArrayList<LLPWorker<SInput>>(latticeValues.length);
             for (int i = 0; i < latticeValues.length; i += 1) {
-                LLPWorker<TInput> workerClone = worker.clone();
-                workerClone.setState(new LLPWorkerState<TInput>(i, latticeValues, input));
+                LLPWorker<SInput> workerClone = worker.clone();
+                workerClone.setState(new LLPWorkerState<SInput>(i, latticeValues, input));
                 workers.set(i, workerClone);
             }
-            return new LLPRunner<TInput, TOutput>(
+            return new LLPRunner<SInput, SOutput>(
                     workers,
                     latticeValues,
                     input,
-                    llpOutputBuilder);
+                    llpOutputBuilder,
+                    numThreads,
+                    dispatchAlgorithm);
         }
     }
 
     /**
-     * Run the LLP algorithm.
+     * Run the LLP algorithm in the background.
      */
     public void computeLLP() {
+        // step 1: partition the workers
+        // step 2: create and start the threads
+    }
 
+    /**
+     * Block the main (or calling) thread until all worker threads have completed.
+     */
+    public void joinAllThreads() throws InterruptedException {
+        for (int i = 0; i < threads.size(); i += 1) {
+            threads.get(i).join();
+        }
     }
 
     /**
