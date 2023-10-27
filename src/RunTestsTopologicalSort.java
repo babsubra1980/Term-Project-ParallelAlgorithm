9e299c7a45f5f27f65d716bc65944dee6832a51f
babsubra1980 <37005639+babsubra1980@users.noreply.github.com>
2023-10-27 14:01:41 -0500

    Update RunTestsTopologicalSort.java

--- src/RunTestsTopologicalSort.java
+++ src/RunTestsTopologicalSort.java
@@ -1,9 +1,59 @@
-public class RunTests {
-	
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+class Graph {
+    private int V; 
+    private static List<List<Integer>> adj;
+
+    public Graph(int v) {
+        V = v;
+        setAdj(new ArrayList<>(V));
+        for (int i = 0; i < V; i++) {
+            getAdj().add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        getAdj().get(u).add(v);
+    }
+
+	public static List<List<Integer>> getAdj() {
+		return adj;
+	}
+
+	public static void setAdj(List<List<Integer>> adj) {
+		Graph.adj = adj;
+	}
+
+    public List<List<Integer>> createPredecessorList() {
+        List<List<Integer>> predecessorList = new ArrayList<>(V);
+
+        for (int i = 0; i < V; i++) {
+            predecessorList.add(new ArrayList<>());
+        }
+
+        for (int u = 0; u < V; u++) {
+            for (int v : adj.get(u)) {
+                predecessorList.get(v).add(u);
+            }
+        }
+
+        return predecessorList;
+    }
+
+    
+}
+
+public class RunTestsTopologicalSort {
+	
     public static void testTopologicalSort(int nodes, List<List<Integer>> list) {
-		TopologicalSort ptsort = new TopologicalSort();
-		
+        System.out.println("**");
+        System.out.println("** ParallelTopologicalSort **");
+    	TopologicalSort ptsort = new TopologicalSort();
+    	System.out.println("**");
 
         try {
 			List<Integer> out = ptsort.execute(list, 2);
@@ -12,40 +62,48 @@ public class RunTests {
                 }
                 System.out.println();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
-    public static void main(String[] args) {
-    	
-  
-        int nodes = 11;
 
-    	
-        List<List<Integer>> adjacencyList = new ArrayList<>();
-        for (int i = 0; i < nodes; i++) {
-            adjacencyList.add(new ArrayList<>());
+	public static void main(String[] args) {
+		
+    	String fileName = "TopoSort.txt";
+    	int lines = 0;
+    	String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)) ) {
+            
+            while ((line = br.readLine()) != null) {
+            	lines++;
+            }
+            br.close();
+         }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        Graph graph = new Graph(lines);
+        line = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)) ) {
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                int node = Integer.parseInt(parts[0]);
+                if (parts.length > 1) {
+                    String[] neighborStrings = parts[1].split(",");
+                    for (String neighborStr : neighborStrings) {
+                        int neighbor = Integer.parseInt(neighborStr);
+                        graph.addEdge(neighbor,node);
+                    }
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        adjacencyList.get(0).add(1);
-        adjacencyList.get(0).add(2);
-        adjacencyList.get(1).add(3);
-        adjacencyList.get(2).add(3);
-        adjacencyList.get(3).add(4);
-        adjacencyList.get(3).add(5);
-        adjacencyList.get(8).add(9);
-        adjacencyList.get(9).add(10);
-        adjacencyList.get(4).add(6);
-        adjacencyList.get(5).add(6);
-        adjacencyList.get(6).add(8);
-        adjacencyList.get(7).add(8);
         
-        testTopologicalSort(nodes, adjacencyList);
-        
-    }
-    
+        testTopologicalSort(lines-1, Graph.getAdj());
 
+	}
 
-
-  	
 }
